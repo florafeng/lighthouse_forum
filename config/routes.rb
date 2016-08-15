c3a34d721ae754d9c70088e93eea448637bfbeac
florafeng <sfeng3@ualberta.ca>
2016-08-15 21:57:50 +0000

    Adding edit post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
 
+  # get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post' # necessary for the update action!
+  patch 'posts/:id', to: 'posts#update'
   post 'posts', to: 'posts#create'
 end
