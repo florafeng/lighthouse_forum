e2981993464cfee4a43316d7a5f88bfa013adbc1
florafeng <sfeng3@ualberta.ca>
2016-08-15 22:16:21 +0000

    adding delete post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  # get 'posts', to: 'posts#index'
+  # get 'posts/new', to: 'posts#new'
 
-  # get 'posts/:id', to: 'posts#show'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id', to: 'posts#show', as: 'post' # necessary for the update action!
-  patch 'posts/:id', to: 'posts#update'
-  post 'posts', to: 'posts#create'
+  # # get 'posts/:id', to: 'posts#show'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # get 'posts/:id', to: 'posts#show', as: 'post' # necessary for the update action!
+  # patch 'posts/:id', to: 'posts#update'
+  # post 'posts', to: 'posts#create'
+  resources :posts
 end
